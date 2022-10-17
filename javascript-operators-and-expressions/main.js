ecc73f89df2dc4585c392d26da94037ccf6bd2d5
Aysha Hussain <ayshafajar3@gmail.com>
2022-10-17 18:30:52 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 2;
+var height = 4;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 14;
+var payment = 5;
+var change = bill - payment;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 78;
+var midterm = 90;
+var final = 98;
+var average = (quizzes + midterm + final) / 3;
+console.log('value of average', average);
+console.log('typeof average:', typeof average);
+
+var firstName = 'Aysha ';
+var lastName = 'Hussain';
+var fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+console.log('value of isAcidic', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 47;
+var isSparta = headCount === 300;
+console.log('vlaue of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
