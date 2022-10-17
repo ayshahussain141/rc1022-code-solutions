ee99d5cab974354e391ebc4eb6ba918fea1d08de
Aysha Hussain <ayshafajar3@gmail.com>
2022-10-17 16:45:37 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,13 @@
+var fullName = 'Aysha Hussain';
+var isCool = true;
+var totalPets = 0;
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
+
+var never;
+console.log(never);
+console.log(typeof never);
