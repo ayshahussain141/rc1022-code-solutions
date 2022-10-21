d56b4c2343a2c31465311fe76d3f9e8c33c61545
Aysha Hussain <ayshafajar3@gmail.com>
2022-10-21 20:25:34 +0000

    completed array-manipulation-1

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,19 @@
+/* exported head */
+
+/*
+//Define a function head with one parameter array an in that function//
+// assign first index of an array to the variable firstIndex//
+return firstIndex
+*/
+
+function head(array) {
+  var firstIndex = array[0];
+  return firstIndex;
+}
+
+/*
+//Define function head with one paramater array//
+//In the function //
+//assign first index of an array to the variable firstIndex//
+//return the firstIndex variable//
+*/
