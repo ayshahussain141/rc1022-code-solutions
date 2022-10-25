536bb2bb0c9f778949a8181ae60c690f3f7de1a4
Aysha Hussain <ayshafajar3@gmail.com>
2022-10-25 03:07:30 +0000

    completed dom-manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,22 @@
+
+var $button = document.querySelector('.hot-button.cold');
+var $count = document.querySelector('.click-count');
+
+$button.addEventListener('click', adding);
+var number = 0;
+function adding() {
+  $count.textContent = number++;
+  if (number < 4) {
+    $button.className = 'hot-button cold';
+  } else if (number < 7) {
+    $button.className = 'hot-button cool';
+  } else if (number < 10) {
+    $button.className = 'hot-button tepid';
+  } else if (number < 13) {
+    $button.className = 'hot-button warm';
+  } else if (number < 16) {
+    $button.className = 'hot-button hot';
+  } else {
+    $button.className = 'hot-button nuclear';
+  }
+}
