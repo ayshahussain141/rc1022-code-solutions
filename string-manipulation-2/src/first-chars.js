9d6e47f3b624da2239bcf0c1f6669a2fe625aebf
Aysha Hussain <ayshafajar3@gmail.com>
2022-10-26 04:52:18 +0000

    completed string-manipulation-2

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -6,3 +6,12 @@ function firstChars(length, string) {
   }
   return word;
 }
+
+/*
+// write function firstChars to two para,eters length and string///
+//make the slice methid of the string object equal to two arguments o and length
+and assign it to variable word//
+write am if statment if length peoperty of the word object is less than or equal to
+length and return string and word//
+return word after the if statment//
+*/
