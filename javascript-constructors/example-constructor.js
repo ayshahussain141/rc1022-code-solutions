2196ade7e4ccbf7aea7b8dc93e55ba3a027f7b27
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-08 18:48:52 +0000

    completed javascript-constructors

---
+++ javascript-constructors/example-constructor.js
@@ -0,0 +1,6 @@
+function ExampleConstructor() {}
+var afunction = new ExampleConstructor();
+console.log(afunction);
+
+var boolean = afunction instanceof ExampleConstructor;
+console.log('function instanceof', boolean);
---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var obj = new Object();
+console.log('value of an object:', obj);
+console.log('type of an object:', typeof obj);
+
+var array = new Array();
+console.log('value of an array:', array);
+console.log('type of array:', typeof array);
+
+var afunction = new Function();
+console.log('value of a Function:', afunction);
+console.log('type of function:', typeof afunction);
+
+var string = new String();
+console.log('value of a string:', string);
+console.log('type of string:', typeof string);
+
+var boolean = new Boolean();
+console.log('value of a boolean:', boolean);
+console.log('type of boolean:', typeof boolean);
