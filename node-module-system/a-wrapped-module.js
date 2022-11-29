d131b681c62b5dbe180dd7866d25993644ab6971
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-29 22:36:18 +0000

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,15 @@
+
+console.log('typeof exports:', typeof exports);
+console.log('value exports:', exports);
+
+console.log('typeof require:', typeof require);
+console.log('value require:', require);
+
+console.log('typeof module:', typeof module);
+console.log('value module:', module);
+
+console.log('typeof filename:', typeof __filename);
+console.log('value filename:', __filename);
+
+console.log('typeof dirname:', typeof __dirname);
+console.log('value dirname:', __dirname);
