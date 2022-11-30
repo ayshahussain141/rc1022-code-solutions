d65a31b76d097e923db53d0ea74f8f4be990a6f3
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-30 03:11:23 +0000

    completed node-require

---
+++ node-require/calculate.js
@@ -0,0 +1,17 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const divide = require('./divide');
+const multiply = require('./multiply');
+const one = process.argv[2];
+const two = process.argv[4];
+if (process.argv[3] === 'minus') {
+  console.log('result:', subtract(one, two));
+} else if (process.argv[3] === 'plus') {
+  console.log('result:', add(one, two));
+} else if (process.argv[3] === 'over') {
+  console.log('result:', divide(one, two));
+} else if (process.argv[3] === 'times') {
+  console.log('result:', multiply(one, two));
+} else {
+  console.log('invalid operation');
+}
---
+++ node-require/divide.js
@@ -0,0 +1,4 @@
+module.exports = function (number1, number2) {
+  return Number(number1) / Number(number2);
+};
+module.exports();
